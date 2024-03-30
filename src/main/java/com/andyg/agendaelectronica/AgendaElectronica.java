2aeb079ad36b190bdb1b40a11e9322d4c48613c3
AndyGarcia <andyg.oyarzo@gmail.com>
2024-03-30 02:14:31 -0300

    first commit

---
+++ src/main/java/com/andyg/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,19 @@
+
+
+package com.andyg.agendaelectronica;
+
+import com.andyg.agendaelectronica.UI.Principal;
+
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        princ.setResizable(false);
+        
+    }
+}
+ 
